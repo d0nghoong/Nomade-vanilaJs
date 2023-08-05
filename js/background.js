9c0a79ebf49eaa9c5d662ecb703b2937c3401719
d0nghoong <slimgreg5208@gmail.com>
2023-08-05 15:10:38 +0900

    배경 수정
    배경 이미지를 수정하였다

--- js/background.js
+++ js/background.js
@@ -2,7 +2,13 @@ const image = ["blue.avif", "green.avif", "sky.avif"];
 
 const chosenImage = image[Math.floor(Math.random() * image.length)];
 
-const bgImage = document.createElement("img");
+/*const bgImage = document.createElement("img");
 
-bgImage.src = "img/" + chosenImage;
-document.body.appendChild(bgImage);
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);*/
+
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundPosition = "top";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundAttachment = "fixed";
