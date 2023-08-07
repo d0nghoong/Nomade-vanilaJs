119d292c69bfa166f57d6a28f6bf1d51cb27a65a
d0nghoong <slimgreg5208@gmail.com>
2023-08-07 22:18:52 +0900

    todo 설정-1
    todo 기능을 설정하기 위한 배경을 완료했다

---
+++ js/todo.js
@@ -0,0 +1,9 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoList = document.getElementById("todo-list");
+const toDoInput = toDoForm.querySelector("input");
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+}
+toDoForm.addEventListener("submit", handleToDoSubmit);
