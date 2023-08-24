fc95d85ba401c5f3da3ae95d43a856daca5d1dab
d0nghoong <slimgreg5208@gmail.com>
2023-08-24 22:57:38 +0900

    todo 리스트 저장 기능
    todo 리스트를 local storage에 넣음으로써 각 항목이 영구적으로 저장되도록 하였다

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,15 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
+
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
@@ -17,9 +26,19 @@ function handleToDoSubmit(event) {
   const newToDo = toDoInput.value;
   toDoInput.value = "";
   paintToDo(newToDo);
+  toDos.push(newToDo);
+  saveToDos();
 }
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
