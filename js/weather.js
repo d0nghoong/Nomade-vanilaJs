1952d8c1647487c80a818b8406620f15506a687f
d0nghoong <slimgreg5208@gmail.com>
2023-09-03 15:24:34 +0900

    weather.js
    날씨 표시 기능을 추가하였다

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,17 @@ const API_KEY = "0309c6643504b993bc5573a5be3a4a39";
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
-  console.log(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const cityContainer = document.querySelector("#weather span:first-child");
+      const weatherContainer = document.querySelector(
+        "#weather span:last-child"
+      );
+      cityContainer.innerText = data.name;
+      weatherContainer.innerText = `${data.weather[0].main}/${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you");
